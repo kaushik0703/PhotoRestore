f555746a1bf201ebf2e00a94757dfb49af1754cc
Kaushik Moralwar <106008167+kaushik0703@users.noreply.github.com>
2024-02-07 17:07:48 +0530

    Update restore.tsx

--- pages/restore.tsx
+++ pages/restore.tsx
@@ -1,46 +1,25 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
-import CountUp from "react-countup";
-import { UploadDropzone } from "react-uploader";
-import { Uploader } from "uploader";
-import { CompareSlider } from "../components/CompareSlider";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import LoadingDots from "../components/LoadingDots";
-import ResizablePanel from "../components/ResizablePanel";
-import Toggle from "../components/Toggle";
-import appendNewToName from "../utils/appendNewToName";
-import downloadPhoto from "../utils/downloadPhoto";
-import NSFWPredictor from "../utils/nsfwCheck";
-import va from "@vercel/analytics";
-
-// Configuration for the uploader
-const uploader = Uploader({
-  apiKey: !!process.env.NEXT_PUBLIC_UPLOAD_API_KEY
-    ? process.env.NEXT_PUBLIC_UPLOAD_API_KEY
-    : "free",
-});
-const options = {
-  maxFileCount: 1,
-  mimeTypes: ["image/jpeg", "image/png", "image/jpg"],
-  editor: { images: { crop: false } },
-  styles: { colors: { primary: "#000" } },
-  onValidate: async (file: File): Promise<undefined | string> => {
-    let isSafe = false;
-    try {
-      isSafe = await NSFWPredictor.isSafeImg(file);
-      if (!isSafe) va.track("NSFW Image blocked");
-    } catch (error) {
-      console.error("NSFW predictor threw an error", error);
-    }
-    return isSafe
-      ? undefined
-      : "Detected a NSFW image which is not allowed. If this was a mistake, please contact me at hassan@hey.com";
-  },
-};
+import { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
+import { UrlBuilder } from '@bytescale/sdk';
+import {
+  UploadWidgetConfig,
+  UploadWidgetOnPreUploadResult,
+} from '@bytescale/upload-widget';
+import { UploadDropzone } from '@bytescale/upload-widget-react';
+import { CompareSlider } from '../components/CompareSlider';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import LoadingDots from '../components/LoadingDots';
+import Toggle from '../components/Toggle';
+import appendNewToName from '../utils/appendNewToName';
+import downloadPhoto from '../utils/downloadPhoto';
+import NSFWFilter from 'nsfw-filter';
+import va from '@vercel/analytics';
+import { useSession, signIn } from 'next-auth/react';
+import useSWR from 'swr';
+import { Rings } from 'react-loader-spinner';
 
 const Home: NextPage = () => {
   const [originalPhoto, setOriginalPhoto] = useState<string | null>(null);
@@ -51,15 +30,57 @@ const Home: NextPage = () => {
   const [error, setError] = useState<string | null>(null);
   const [photoName, setPhotoName] = useState<string | null>(null);
 
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, mutate } = useSWR('/api/remaining', fetcher);
+  const { data: session, status } = useSession();
+
+  const options: UploadWidgetConfig = {
+    apiKey: !!process.env.NEXT_PUBLIC_UPLOAD_API_KEY
+      ? process.env.NEXT_PUBLIC_UPLOAD_API_KEY
+      : 'free',
+    maxFileCount: 1,
+    mimeTypes: ['image/jpeg', 'image/png', 'image/jpg'],
+    editor: { images: { crop: false } },
+    styles: { colors: { primary: '#000' } },
+    onPreUpload: async (
+      file: File
+    ): Promise<UploadWidgetOnPreUploadResult | undefined> => {
+      let isSafe = false;
+      try {
+        isSafe = await NSFWFilter.isSafe(file);
+        console.log({ isSafe });
+        if (!isSafe) va.track('NSFW Image blocked');
+      } catch (error) {
+        console.error('NSFW predictor threw an error', error);
+      }
+      if (!isSafe) {
+        return { errorMessage: 'Detected a NSFW image which is not allowed.' };
+      }
+      if (data.remainingGenerations === 0) {
+        return { errorMessage: 'No more generations left for the day.' };
+      }
+      return undefined;
+    },
+  };
+
   const UploadDropZone = () => (
     <UploadDropzone
-      uploader={uploader}
       options={options}
-      onUpdate={(file) => {
-        if (file.length !== 0) {
-          setPhotoName(file[0].originalFile.originalFileName);
-          setOriginalPhoto(file[0].fileUrl.replace("raw", "thumbnail"));
-          generatePhoto(file[0].fileUrl.replace("raw", "thumbnail"));
+      onUpdate={({ uploadedFiles }) => {
+        if (uploadedFiles.length !== 0) {
+          const image = uploadedFiles[0];
+          const imageName = image.originalFile.originalFileName;
+          const imageUrl = UrlBuilder.url({
+            accountId: image.accountId,
+            filePath: image.filePath,
+            options: {
+              transformation: 'preset',
+              transformationPreset: 'thumbnail',
+            },
+          });
+          setPhotoName(imageName);
+          setOriginalPhoto(imageUrl);
+          generatePhoto(imageUrl);
         }
       }}
       width="670px"
@@ -70,10 +91,11 @@ const Home: NextPage = () => {
   async function generatePhoto(fileUrl: string) {
     await new Promise((resolve) => setTimeout(resolve, 500));
     setLoading(true);
-    const res = await fetch("/api/generate", {
-      method: "POST",
+
+    const res = await fetch('/api/generate', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ imageUrl: fileUrl }),
     });
@@ -82,6 +104,7 @@ const Home: NextPage = () => {
     if (res.status !== 200) {
       setError(newPhoto);
     } else {
+      mutate();
       setRestoredImage(newPhoto);
     }
     setLoading(false);
@@ -94,125 +117,180 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
+      <Header photo={session?.user?.image || undefined} />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-4 sm:mb-0 mb-8">
         <a
-          href="https://github.com/kaushik0703"
+          className="border shadow-xl flex max-w-md rounded-xl mb-6 hover:scale-[1.02] transition duration-300 ease-in-out"
+          href="https://www.roomgpt.io/"
           target="_blank"
-          rel="noreferrer"
-          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
+          onClick={() => va.track('RoomGPT link clicked')}
         >
-          Enjoyed this one? Want to see my other projects{" "}
-          <span className="font-bold">Click Here</span>.
+          <img
+            src="/roomgpt-ad.png"
+            alt="roomgpt ad"
+            className="w-48 rounded-lg"
+          />
+          <div className="flex gap-3 flex-col py-3 sm:pr-4 pr-2">
+            <h3 className="text-left sm:text-md text-sm text-gray-700">
+              Revolutionize your space with the world's first AI interior
+              designer, 100% free to try.{' '}
+            </h3>
+            <p className="text-left sm:text-sm text-xs text-gray-500 opacity-50 font-medium">
+              ROOMGPT.IO
+            </p>
+          </div>
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-4xl font-bold tracking-normal text-slate-900 sm:text-6xl mb-5">
           Restore any face photo
         </h1>
-        <p className="text-slate-500">
-          {" "}
-          <CountUp start={10} end={251} duration={2} separator="," />{" "}
-          photos generated and counting.
-        </p>
-        <ResizablePanel>
-          <AnimatePresence exitBeforeEnter>
-            <motion.div className="flex justify-between items-center w-full flex-col mt-4">
-              <Toggle
-                className={`${restoredLoaded ? "visible" : "invisible"} mb-6`}
-                sideBySide={sideBySide}
-                setSideBySide={(newVal) => setSideBySide(newVal)}
+        {status === 'authenticated' && data && (
+          <p className="text-slate-500">
+            You have{' '}
+            <span className="font-semibold">
+              {data.remainingGenerations} generations
+            </span>{' '}
+            left today. Your generation
+            {Number(data.remainingGenerations) > 1 ? 's' : ''} will renew in{' '}
+            <span className="font-semibold">
+              {data.hours} hours and {data.minutes} minutes.
+            </span>
+          </p>
+        )}
+        <div className="flex justify-between items-center w-full flex-col mt-4">
+          <Toggle
+            className={`${restoredLoaded ? 'visible mb-6' : 'invisible'}`}
+            sideBySide={sideBySide}
+            setSideBySide={(newVal) => setSideBySide(newVal)}
+          />
+          {restoredLoaded && sideBySide && (
+            <CompareSlider
+              original={originalPhoto!}
+              restored={restoredImage!}
+            />
+          )}
+          {status === 'loading' ? (
+            <div className="max-w-[670px] h-[250px] flex justify-center items-center">
+              <Rings
+                height="100"
+                width="100"
+                color="black"
+                radius="6"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="rings-loading"
               />
-              {restoredLoaded && sideBySide && (
-                <CompareSlider
-                  original={originalPhoto!}
-                  restored={restoredImage!}
-                />
-              )}
-              {!originalPhoto && <UploadDropZone />}
-              {originalPhoto && !restoredImage && (
+            </div>
+          ) : status === 'authenticated' && !originalPhoto ? (
+            <UploadDropZone />
+          ) : (
+            !originalPhoto && (
+              <div className="h-[250px] flex flex-col items-center space-y-6 max-w-[670px] -mt-8">
+                <div className="max-w-xl text-gray-600">
+                  Sign in below with Google to create a free account and restore
+                  your photos today. You will be able to restore 5 photos per
+                  day for free.
+                </div>
+                <button
+                  onClick={() => signIn('google')}
+                  className="bg-gray-200 text-black font-semibold py-3 px-6 rounded-2xl flex items-center space-x-2"
+                >
+                  <Image
+                    src="/google.png"
+                    width={20}
+                    height={20}
+                    alt="google's logo"
+                  />
+                  <span>Sign in with Google</span>
+                </button>
+              </div>
+            )
+          )}
+          {originalPhoto && !restoredImage && (
+            <Image
+              alt="original photo"
+              src={originalPhoto}
+              className="rounded-2xl"
+              width={475}
+              height={475}
+            />
+          )}
+          {restoredImage && originalPhoto && !sideBySide && (
+            <div className="flex sm:space-x-4 sm:flex-row flex-col">
+              <div>
+                <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
                 <Image
                   alt="original photo"
                   src={originalPhoto}
-                  className="rounded-2xl"
+                  className="rounded-2xl relative"
                   width={475}
                   height={475}
                 />
-              )}
-              {restoredImage && originalPhoto && !sideBySide && (
-                <div className="flex sm:space-x-4 sm:flex-row flex-col">
-                  <div>
-                    <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
-                    <Image
-                      alt="original photo"
-                      src={originalPhoto}
-                      className="rounded-2xl relative"
-                      width={475}
-                      height={475}
-                    />
-                  </div>
-                  <div className="sm:mt-0 mt-8">
-                    <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
-                    <a href={restoredImage} target="_blank" rel="noreferrer">
-                      <Image
-                        alt="restored photo"
-                        src={restoredImage}
-                        className="rounded-2xl relative sm:mt-0 mt-2 cursor-zoom-in"
-                        width={475}
-                        height={475}
-                        onLoadingComplete={() => setRestoredLoaded(true)}
-                      />
-                    </a>
-                  </div>
-                </div>
-              )}
-              {loading && (
-                <button
-                  disabled
-                  className="bg-black rounded-full text-white font-medium px-4 pt-2 pb-3 mt-8 hover:bg-black/80 w-40"
-                >
-                  <span className="pt-4">
-                    <LoadingDots color="white" style="large" />
-                  </span>
-                </button>
-              )}
-              {error && (
-                <div
-                  className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-xl mt-8"
-                  role="alert"
-                >
-                  <span className="block sm:inline">{error}</span>
-                </div>
-              )}
-              <div className="flex space-x-2 justify-center">
-                {originalPhoto && !loading && (
-                  <button
-                    onClick={() => {
-                      setOriginalPhoto(null);
-                      setRestoredImage(null);
-                      setRestoredLoaded(false);
-                      setError(null);
-                    }}
-                    className="bg-black rounded-full text-white font-medium px-4 py-2 mt-8 hover:bg-black/80 transition"
-                  >
-                    Upload New Photo
-                  </button>
-                )}
-                {restoredLoaded && (
-                  <button
-                    onClick={() => {
-                      downloadPhoto(
-                        restoredImage!,
-                        appendNewToName(photoName!)
-                      );
-                    }}
-                    className="bg-white rounded-full text-black border font-medium px-4 py-2 mt-8 hover:bg-gray-100 transition"
-                  >
-                    Download Restored Photo
-                  </button>
-                )}
               </div>
-            </motion.div>
-          </AnimatePresence>
-        </ResizablePanel>
+              <div className="sm:mt-0 mt-8">
+                <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                <a href={restoredImage} target="_blank" rel="noreferrer">
+                  <Image
+                    alt="restored photo"
+                    src={restoredImage}
+                    className="rounded-2xl relative sm:mt-0 mt-2 cursor-zoom-in"
+                    width={475}
+                    height={475}
+                    onLoadingComplete={() => setRestoredLoaded(true)}
+                  />
+                </a>
+              </div>
+            </div>
+          )}
+          {loading && (
+            <button
+              disabled
+              className="bg-black rounded-full text-white font-medium px-4 pt-2 pb-3 mt-8 hover:bg-black/80 w-40"
+            >
+              <span className="pt-4">
+                <LoadingDots color="white" style="large" />
+              </span>
+            </button>
+          )}
+          {error && (
+            <div
+              className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-xl mt-8 max-w-[575px]"
+              role="alert"
+            >
+              <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                Please try again in 24 hours
+              </div>
+              <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                {error}
+              </div>
+            </div>
+          )}
+          <div className="flex space-x-2 justify-center">
+            {originalPhoto && !loading && (
+              <button
+                onClick={() => {
+                  setOriginalPhoto(null);
+                  setRestoredImage(null);
+                  setRestoredLoaded(false);
+                  setError(null);
+                }}
+                className="bg-black rounded-full text-white font-medium px-4 py-2 mt-8 hover:bg-black/80 transition"
+              >
+                Upload New Photo
+              </button>
+            )}
+            {restoredLoaded && (
+              <button
+                onClick={() => {
+                  downloadPhoto(restoredImage!, appendNewToName(photoName!));
+                }}
+                className="bg-white rounded-full text-black border font-medium px-4 py-2 mt-8 hover:bg-gray-100 transition"
+              >
+                Download Restored Photo
+              </button>
+            )}
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
